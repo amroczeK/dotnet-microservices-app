61ce79c3496a2a845b0979a789fe391dede58c1d
amroczeK <mroczek9990@gmail.com>
2022-03-04 23:03:00 +0800

    basic platform controller to test inbound conn

---
+++ CommandsService/Controllers/PlatformsController.cs
@@ -0,0 +1,21 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CommandsService.Controllers
+{
+    [Route("api/c/[controller]")]
+    [ApiController]
+    public class PlatformsController : ControllerBase
+    {
+        public PlatformsController()
+        {
+            
+        }
+
+        [HttpPost]
+        public ActionResult TestInboundConnection(){
+            Console.WriteLine("--> Inhound POST # Command Service");
+            return Ok("Inhound test ok platforms controller");
+        }
+    }
+}
