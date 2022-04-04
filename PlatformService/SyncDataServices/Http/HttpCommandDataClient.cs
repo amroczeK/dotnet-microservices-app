7dcef05643c03ec293d3f7ae5ff0341a548f93dc
amroczeK <mroczek9990@gmail.com>
2022-04-04 10:39:26 +0800

    re-add PlatformService as submodule, link issue

---
+++ PlatformService/SyncDataServices/Http/HttpCommandDataClient.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using PlatformService.Dtos;
+
+namespace PlatformService.SyncDataServices.Http
+{
+    public class HttpCommandDataClient : ICommandDataClient
+    {
+        private readonly HttpClient _httpCLient;
+        private readonly IConfiguration _configuration;
+
+        public HttpCommandDataClient(HttpClient httpClient, IConfiguration configuration)
+        {
+            _httpCLient = httpClient;
+            _configuration = configuration;
+        }
+
+        public async Task SendPlatformToCommand(PlatformReadDto plat)
+        {
+           var httpContent = new StringContent(
+               JsonSerializer.Serialize(plat),
+               Encoding.UTF8,
+               "application/json"
+           );
+
+           var response = await _httpCLient.PostAsync($"{_configuration["CommandService"]}/api/c/platforms", httpContent);
+
+           if(response.IsSuccessStatusCode){
+               Console.WriteLine("--> Sync POST to CommandService was OK!");
+           }
+           else {
+                Console.WriteLine("--> Sync POST to CommandService failed!");
+           }
+        }
+    }
+}
